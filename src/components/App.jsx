3dc6dca2c583b23dc8e72c02f9ec2fcf5ab0c971
Ihor Karen <karenihor@gmail.com>
2024-01-19 21:50:59 +0200

    sketch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,43 +1,110 @@
-import { useRef, useEffect, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
-
+import Map, { Marker, Source, Layer } from 'react-map-gl';
+import { useState, useRef } from 'react';
 import { Container } from './Container/Container.styled';
 
-mapboxgl.accessToken =
-  'pk.eyJ1IjoiaWhvcmthcmVuIiwiYSI6ImNscmtraGp5NjA5ZGQya3F6bzNhcm5wdGMifQ.2TIRinxIjYANsJUWnyWkBg';
+const data = [
+  {
+    address: 'Námestie SNP 1, 811 01 Bratislava, Slovensko',
+    latitude: 48.145974,
+    longitude: 17.107218,
+  },
+  {
+    address: 'Hviezdoslavovo námestie 1, 811 01 Bratislava, Slovensko',
+    latitude: 48.145934,
+    longitude: 17.107315,
+  },
+  {
+    address: 'Nábrežie Dunaja 1, 811 01 Bratislava, Slovensko',
+    latitude: 48.144715,
+    longitude: 17.107934,
+  },
+  {
+    address: 'Panská ulica 2, 811 01 Bratislava, Slovensko',
+    latitude: 48.145965,
+    longitude: 17.107325,
+  },
+  {
+    address: 'Mlynské nivy 1, 811 01 Bratislava, Slovensko',
+    latitude: 48.145565,
+    longitude: 17.108025,
+  },
+];
 
 export const App = () => {
-  const mapContainer = useRef(null);
-  const map = useRef(null);
-  // eslint-disable-next-line no-unused-vars
-  const [lng, setLng] = useState(17.1077);
-  // eslint-disable-next-line no-unused-vars
-  const [lat, setLat] = useState(48.1486);
-  // eslint-disable-next-line no-unused-vars
-  const [zoom, setZoom] = useState(15);
-
-  useEffect(() => {
-    if (map.current) return;
-    map.current = new mapboxgl.Map({
-      container: mapContainer.current,
-      style: 'mapbox://styles/mapbox/streets-v12',
-      center: [lng, lat],
-      zoom: zoom,
-    });
-
-    map.current.on('move', () => {
-      setLng(map.current.getCenter().lng.toFixed(4));
-      setLat(map.current.getCenter().lat.toFixed(4));
-      setZoom(map.current.getZoom().toFixed(2));
-    });
-  });
+  const mapRef = useRef(null);
 
   return (
     <Container>
-      <div className="sidebar">
-        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
-      </div>
-      <div ref={mapContainer} className="map-container" />
+      <Map
+        mapboxAccessToken={process.env.REACT_APP_MAP_ACCESS_KEY}
+        mapLib={import('mapbox-gl')}
+        initialViewState={{
+          longitude: 17.1077,
+          latitude: 48.1486,
+          zoom: 15,
+        }}
+        ref={instance => (mapRef.current = instance)}
+        width="100%"
+        height="900px"
+        mapStyle="mapbox://styles/mapbox/streets-v12"
+      >
+        {data.map(house => (
+          <Marker
+            key={house.address}
+            latitude={house.latitude}
+            longitude={house.longitude}
+            offsetLeft={-15}
+            offsetTop={-15}
+          >
+            <button
+              style={{ width: '30px', height: '30px', fontSize: '30px' }}
+              type="button"
+            >
+              <img
+                width={20}
+                src={'../../public/logo192.png'}
+                alt="house"
+                className="w-8"
+              />
+            </button>
+          </Marker>
+        ))}
+      </Map>
     </Container>
   );
 };
+
+// const geojson = {
+//   type: 'FeatureCollection',
+//   features: [
+//     {
+//       type: 'Feature',
+//       id: 1,
+//       geometry: { type: 'Point', coordinates: [48.145565, 17.108025] },
+//     },
+//     {
+//       type: 'Feature',
+//       id: 2,
+//       geometry: { type: 'Point', coordinates: [48.145965, 17.107325] },
+//     },
+//     {
+//       type: 'Feature',
+//       id: 3,
+//       geometry: { type: 'Point', coordinates: [17.1077, 48.1486] },
+//     },
+//   ],
+// };
+
+// const layerStyle = {
+//   id: 'point',
+//   type: 'fill',
+//   source: 'mapbox',
+//   filter: ['==', 'class', 'park'],
+//   paint: {
+//     'fill-color': '#4E3FC8',
+//   },
+// };
+
+// <Source id="my-data" type="geojson" data={geojson}>
+// <Layer {...layerStyle} />
+// </Source>
--- src/components/Container/Container.styled.jsx
+++ src/components/Container/Container.styled.jsx
@@ -1,8 +1,11 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
   display: flex;
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  width: 100%;
+  height: 900px;
 `;
