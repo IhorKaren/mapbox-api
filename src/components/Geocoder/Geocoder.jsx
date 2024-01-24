acd0b06751d154b40e5586b9698687b78543602b
Ihor Karen <karenihor@gmail.com>
2024-01-24 19:57:45 +0200

    maplibre

--- src/components/Geocoder/Geocoder.jsx
+++ src/components/Geocoder/Geocoder.jsx
@@ -1,13 +1,48 @@
 import { useControl } from "react-map-gl";
-import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder";
+import MaplibreGeocoder from "@maplibre/maplibre-gl-geocoder";
+import "@maplibre/maplibre-gl-geocoder/dist/maplibre-gl-geocoder.css";
+
+const Geo = {
+  forwardGeocode: async (config) => {
+    const features = [];
+    try {
+      const request = `https://nominatim.openstreetmap.org/search?q=${config.query}&format=geojson&polygon_geojson=1&addressdetails=1`;
+      const response = await fetch(request);
+      const geojson = await response.json();
+      for (const feature of geojson.features) {
+        const center = [
+          feature.bbox[0] + (feature.bbox[2] - feature.bbox[0]) / 2,
+          feature.bbox[1] + (feature.bbox[3] - feature.bbox[1]) / 2,
+        ];
+        const point = {
+          type: "Feature",
+          geometry: {
+            type: "Point",
+            coordinates: center,
+          },
+          place_name: feature.properties.display_name,
+          properties: feature.properties,
+          text: feature.properties.display_name,
+          place_type: ["place"],
+          center,
+        };
+        features.push(point);
+      }
+    } catch (e) {
+      console.error(`Failed to forwardGeocode with error: ${e}`);
+    }
+
+    return {
+      features,
+    };
+  },
+};
 
 const Geocoder = ({ position }) => {
+  // eslint-disable-next-line no-unused-vars
   const geocoder = useControl(
     () => {
-      const ctrl = new MapboxGeocoder({
-        marker: false,
-        accessToken: process.env.REACT_APP_MAP_ACCESS_KEY,
-      });
+      const ctrl = new MaplibreGeocoder(Geo);
 
       return ctrl;
     },
