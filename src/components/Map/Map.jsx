98b1719841e019b912a44660a3eb9ba4c27de013
Orest Klymko <116631168+OrestKlymko@users.noreply.github.com>
2024-01-28 18:03:57 +0100

    update logic

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,8 +4,7 @@ import "mapbox-gl/dist/mapbox-gl.css";
 
 const MapComponent = () => {
     useEffect(() => {
-        mapboxgl.accessToken =
-            "";
+        mapboxgl.accessToken ="pk.eyJ1Ijoib3Jlc3RrbHlta28iLCJhIjoiY2xyYzR2eDJsMHhyNzJpcWltdzJnMzBlaSJ9.HR-Ok3T9DBHN-t-hH-ei_Q";
 
         const map = new mapboxgl.Map({
             container: "map",
@@ -32,7 +31,7 @@ const MapComponent = () => {
                     "source-layer": "building",
                     filter: ["==", "extrude", "true"],
                     type: "fill-extrusion",
-                    minzoom: 15,
+                    minzoom: 24,
                     paint: {
                         "fill-extrusion-color": "#aaa",
                         "fill-extrusion-height": [
@@ -60,30 +59,30 @@ const MapComponent = () => {
             );
 
             // setTimeout(() => {
-                const longitude = 17.1047098672259;
-                const latitude = 48.1027321;
-
-                var point = map.project([longitude, latitude]);
-                console.log(point);
-
-
-                const marker = new mapboxgl.Marker()
-                    .setLngLat([longitude, latitude]) // Встановлюємо координати маркера
-                    .addTo(map); // Додаємо маркер на карту
-
-                const features = map.queryRenderedFeatures(
-                   point,
-                    {layers: ['3d-buildings']}
-                );
-
-
-                console.log(features);
-                if (!features.length) {
-                    console.log("No features found at this location.");
-                    return null;
-                }
+            //     const longitude = 17.1047098672259;
+            //     const latitude = 48.1027321;
+            //
+            //     var point = map.project([longitude, latitude]);
+            //     console.log(point);
+            //
+            //
+            //     const marker = new mapboxgl.Marker()
+            //         .setLngLat([longitude, latitude]) // Встановлюємо координати маркера
+            //         .addTo(map); // Додаємо маркер на карту
+            //
+            //     const features = map.queryRenderedFeatures(
+            //        point,
+            //         {layers: ['3d-buildings']}
+            //     );
+            //
+            //
+            //     console.log(features);
+            //     if (!features.length) {
+            //         console.log("No features found at this location.");
+            //         return null;
+            //     }
                 //////////////////////////////////////////////////////////////////////
-                var buildId = 169621165;
+                var buildId = 53778780;
                 ////////////////////////////////////////////////////
                 //aaadress
                 // var coordinates = features[0].geometry.coordinates;
@@ -105,17 +104,36 @@ const MapComponent = () => {
                 });
 
                 // Inside the useEffect block
+            map.on('idle', function() {
+                var featuresFind = map.queryRenderedFeatures({ layers: ['highlighted-building'] });
+                if (featuresFind.length > 0) {
+                    if (featuresFind[0].geometry && featuresFind[0].geometry.coordinates) {
+                        var coordinates = featuresFind[0].geometry.coordinates;
+                        console.log(coordinates);
+                    }
+                }
+            });
+
+            // map.on('render')
 
 
-                //2
+            //2
                 //Повернути назад геометрію у вигляді масиву координат і висоту _z
 
-            // }, 1000)
-            console.log(map.getLayer("highlighted-building"));
-            var layer1 = map.getLayer("highlighted-building");
+
+            // if (featuresFind.length > 0) {
+            //     // Перевірка на наявність координат у першого елемента
+            //     if (featuresFind[0].geometry && featuresFind[0].geometry.coordinates) {
+            //         var coordinates = featuresFind[0].geometry.coordinates;
+            //         console.log(coordinates);
+            //     }
+            // }
 
 
         })
+
+
+
         map.on("click", function (e) {
             const features = map.queryRenderedFeatures(e.point, {
                 layers: ["3d-buildings"]
