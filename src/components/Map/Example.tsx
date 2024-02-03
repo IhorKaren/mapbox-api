270c4f7a0b4b0fb437a4515fdb823d7e5d9bf004
Ihor Karen <karenihor@gmail.com>
2024-02-03 11:23:18 +0200

    test

---
+++ src/components/Map/Example.tsx
@@ -0,0 +1,64 @@
+// import React, { useEffect } from "react";
+// import MapLibreGL from "maplibre-gl";
+// import "maplibre-gl/dist/maplibre-gl.css";
+
+// const MapComponent = () => {
+//   useEffect(() => {
+//     const map = new MapLibreGL.Map({
+//       container: "map",
+//       center: [17.107, 48.145],
+//       zoom: 15.5,
+//       pitch: 45,
+//       bearing: -17.6,
+//       antialias: true,
+//       style:
+//         "https://api.maptiler.com/maps/streets-v2/style.json?key=jkyOg7nIBllWn5tcjnsS",
+//     });
+
+//     map.on("load", () => {
+//       const layers = map
+//         .getStyle()
+//         .layers.find(
+//           (layer) => layer.type === "symbol" && layer.layout["text-field"]
+//         )?.id;
+//       map.addSource("openmaptiles", {
+//         type: "vector",
+//         url: "https://api.maptiler.com/tiles/v3/tiles.json?key=jkyOg7nIBllWn5tcjnsS",
+//       });
+//       const buildId = 230491553;
+//       map.addLayer({
+//         id: "buildings-3d",
+//         type: "fill-extrusion",
+//         source: "openmaptiles",
+//         "source-layer": "building",
+//         metadata: {},
+//         minzoom: 14,
+//         filter: ["==", ["id"], buildId],
+//         paint: {
+//           "fill-extrusion-color": "#ff0000",
+//           "fill-extrusion-height": ["get", "render_height"],
+//           "fill-extrusion-base": ["get", "render_min_height"],
+//           "fill-extrusion-opacity": 0.3,
+//         },
+//       });
+//     });
+//     map.on("idle", (ev) => {
+//       var queryRenderedFeatures1 = map.queryRenderedFeatures({
+//         layers: ["buildings-3d"],
+//       });
+//       if (queryRenderedFeatures1.length > 0) {
+//         console.log(queryRenderedFeatures1[0].id);
+//         console.log(queryRenderedFeatures1[0].geometry.coordinates);
+//       }
+//     });
+//   }, []);
+
+//   return (
+//     <div
+//       id="map"
+//       style={{ position: "absolute", top: 0, bottom: 0, width: "100%" }}
+//     ></div>
+//   );
+// };
+
+// export default MapComponent;
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -11,7 +11,28 @@ import geoJSON from "../../data/houses_of_bratislava.json";
 import MarkerComponent from "../Marker/Marker";
 import Geocoder from "components/Geocoder/Geocoder";
 
-console.log(geoJSON);
+const geojson = {
+  type: "vector",
+  url: "https://api.maptiler.com/tiles/v3/tiles.json?key=jkyOg7nIBllWn5tcjnsS",
+};
+
+const buildId = 230491553;
+
+const parkLayer = {
+  id: "buildings-3d",
+  type: "fill-extrusion",
+  source: "openmaptiles",
+  "source-layer": "building",
+  metadata: {},
+  minzoom: 14,
+  filter: ["==", ["id"], buildId],
+  paint: {
+    "fill-extrusion-color": "#ff0000",
+    "fill-extrusion-height": ["get", "render_height"],
+    "fill-extrusion-base": ["get", "render_min_height"],
+    "fill-extrusion-opacity": 0.3,
+  },
+};
 
 const MapComponent = ({ data }) => {
   // eslint-disable-next-line no-unused-vars
@@ -27,23 +48,6 @@ const MapComponent = ({ data }) => {
     return parsedNumber;
   };
 
-  // const staticPolygonData = {
-  //   type: "Feature",
-  //   geometry: {
-  //     type: "Polygon",
-  //     coordinates: [
-  //       [
-  //         [17.107, 48.145],
-  //         [17.108, 48.145],
-  //         [17.108, 48.146],
-  //         [17.107, 48.146],
-  //         [17.107, 48.145],
-  //       ],
-  //     ],
-  //   },
-  //   properties: {},
-  // };
-
   return (
     <Map
       initialViewState={{
@@ -55,32 +59,6 @@ const MapComponent = ({ data }) => {
       mapStyle="https://api.maptiler.com/maps/streets-v2/style.json?key=jkyOg7nIBllWn5tcjnsS"
       style={{ width: "100%", height: 900 }}
     >
-      {/* <Source id="static-polygon" type="geojson" data={staticPolygonData}>
-        <Layer
-          id="static-polygon-layer"
-          type="fill"
-          paint={{
-            "fill-color": "#088",
-            "fill-opacity": 0.5,
-          }}
-        />
-      </Source> */}
-      {/* {data.length > 0 &&
-        data.map((el) => (
-          <Marker
-            key={el.id}
-            longitude={numberParse(el.geoPoints.longitude)}
-            latitude={numberParse(el.geoPoints.latitude)}
-            anchor="bottom"
-          >
-            <MarkerComponent
-              longitude={numberParse(el.geoPoints.longitude)}
-              latitude={numberParse(el.geoPoints.latitude)}
-              price={el.price}
-              address={el.title}
-            />
-          </Marker>
-        ))} */}
       {geoJSON.features
         .filter((el) => el.geoPoints)
         .map((el) => (
@@ -98,6 +76,10 @@ const MapComponent = ({ data }) => {
             />
           </Marker>
         ))}
+
+      <Source id="my-data" type="geojson" data={geojson}>
+        <Layer {...parkLayer} />
+      </Source>
       <Geocoder position="top-left" />
       <GeolocateControl />
       <NavigationControl />
